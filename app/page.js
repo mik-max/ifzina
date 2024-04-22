b240e4852c7d3b3bd3468c61929c57dc915a1ac4
David <emekachinye2020@gmail.com>
2024-04-22 17:43:47 +0100

    modified page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 import Banner from "@/components/Banner";
 import Image from "next/image";
 
+
 export default function Home() {
+  
   return (
     <section>
       <Banner first={<><h1>Worlds Biggest University</h1>
