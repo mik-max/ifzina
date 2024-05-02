5718ad07ac66c3b0ec799e0765c791ba717df51a
Michael chinye <michaelchinye2018@gmail.com>
2024-05-02 13:48:40 +0100

    chore: changed footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 
   title: "IFZINA: Premier EPC Partner in Nigeria | Engineering, Procurement, and Construction, Maintenance",
   description: "Learn about IFZINA, a leading EPC company in Nigeria, offering comprehensive engineering, procurement, and construction services. Discover our mission, values, portfolio, and client testimonials.",
-  keywords:"EPC company, engineering services, Nigeria, sustainable solutions, renewable energy, solar, wind energy, building services, maintenance"
+  keywords:"EPC company, engineering services, Nigeria, sustainable solutions, renewable energy, solar, wind energy, building services, maintenance",
 };
 
 export default function RootLayout({ children }) {
--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -12,39 +12,39 @@ import Link from "next/link";
 const Footer = () => {
   return (
 
-     <footer className="w-full lg:px-[100px] md:px-[50px] px-[25px] py-[50px] bg-black-300">
+     <footer className="w-full lg:px-[100px] md:px-[50px] px-[25px] py-[50px] bg-[#EFF1F6]">
           
           <div className="flex md:flex-row flex-col lg:gap-20 md:gap-10">
                <div className="grid lg:grid-cols-2 grid-cols-1 md:gap-20 gap-8 w-[50%]">
                     <div>
-                         <Image src={"/images/logo.png"} width={200} height={200} alt="logo"/>
+                         <Image src={"/images/ifzina_logo.png"} width={200} height={200} alt="logo"/>
                     </div>
                     <div className="lg:mt-5">
-                         <h4 className="text-white text-2xl font-semibold ">Quick Links</h4>
+                         <h4 className="text-[#131316] text-2xl font-semibold ">Quick Links</h4>
                          <ul className={styles.linkList}>
                          <li >
-                              <a href="/" className=" text-md text-[#56616B] hover:text-white  cursor-pointer  hover:underline">Home</a>
+                              <a href="/" className=" text-md text-[#56616B] hover:text-[#131316]  cursor-pointer  hover:underline">Home</a>
                          </li>
                          <li >
-                              <a href="/service" className=" text-md text-[#56616B] hover:text-white  cursor-pointer  hover:underline">Services</a>
+                              <a href="/service" className=" text-md text-[#56616B] hover:text-[#131316]  cursor-pointer  hover:underline">Services</a>
                          </li>
                          <li>
-                              <a href="/about" className=" text-md text-[#56616B] hover:text-white  cursor-pointer  hover:underline">About</a>
+                              <a href="/about" className=" text-md text-[#56616B] hover:text-[#131316]  cursor-pointer  hover:underline">About</a>
                          </li>
                          <li>
-                              <a href="/market" className=" text-md text-[#56616B] hover:text-white cursor-pointer  hover:underline">Market</a>
+                              <a href="/market" className=" text-md text-[#56616B] hover:text-[#131316] cursor-pointer  hover:underline">Market</a>
                          </li>
                          <li>
-                              <a href="/career" className=" text-md text-[#56616B] hover:text-white cursor-pointer  hover:underline">Career</a>
+                              <a href="/career" className=" text-md text-[#56616B] hover:text-[#131316] cursor-pointer  hover:underline">Career</a>
                          </li>
                          <li >
-                              <a href="/contact" className=" text-md text-[#56616B] cursor-pointer  hover:underline hover:text-white">Contact Us</a>
+                              <a href="/contact" className=" text-md text-[#56616B] cursor-pointer  hover:underline hover:text-[#131316]">Contact Us</a>
                          </li>
                          </ul>
                     </div>
                </div>
                <div className="mt-5">
-                    <p className="text-white text-sm font-bold tracking-wide mb-5">Ready to partner with us? Get in touch with our team today to discuss how IFZINA can support your next project.  </p>
+                    <p className="text-[#131316] text-sm font-bold tracking-wide mb-5">Ready to partner with us? Get in touch with our team today to discuss how IFZINA can support your next project.  </p>
                     <p className="text-[#56616B] text-sm leading-7 "><span>Address:</span> 11 Odetola Avenue, Alagbado Lagos, Nigeria </p>
                
                     <p className="text-[#56616B]  text-sm leading-7" ><span>Phone:</span> <a href="tel:+2348034090741" target="_blank">+234-803-4090741 </a></p>
@@ -53,31 +53,31 @@ const Footer = () => {
                     <div className="flex space-x-3 mt-4 ">
                          <Link
                                href=""
-                              className={` flex justify-center items-center right-4 bottom-4  w-12 rounded-full bg-white  z-50 h-12`}
+                              className={` flex justify-center items-center right-4 bottom-4  w-12 rounded-full bg-black-300  z-50 h-12`}
                               target={"_blank"}
                          >
-                              <FiInstagram  color="#000" className="w-6 h-6"  />
+                              <FiInstagram  color="#fff" className="w-6 h-6"  />
                          </Link>
                          <Link
                                href=""
-                              className={` flex justify-center items-center right-4 bottom-4  w-12 rounded-full bg-white  z-50 h-12 `}
+                              className={` flex justify-center items-center right-4 bottom-4  w-12 rounded-full bg-black-300  z-50 h-12 `}
                               target={"_blank"}
                          >
-                              <FaTwitter  color="#000" className="w-6 h-6"  />
+                              <FaTwitter  color="#fff" className="w-6 h-6"  />
                          </Link>
                          <Link
                                href=""
-                              className={` flex justify-center items-center right-4 bottom-4  w-12 rounded-full bg-white  z-50 h-12`}
+                              className={` flex justify-center items-center right-4 bottom-4  w-12 rounded-full bg-black-300  z-50 h-12`}
                               target={"_blank"}
                          >
-                              <FaLinkedinIn  color="#000" className="w-6 h-6"  />
+                              <FaLinkedinIn  color="#fff" className="w-6 h-6"  />
                          </Link>
                          <Link
                                href=""
-                              className={` flex justify-center items-center right-4 bottom-4  w-12 rounded-full bg-white  z-50 h-12`}
+                              className={` flex justify-center items-center right-4 bottom-4  w-12 rounded-full bg-black-300  z-50 h-12`}
                               target={"_blank"}
                          >
-                              <FaFacebookF  color="#000" className="w-6 h-6"  />
+                              <FaFacebookF  color="#fff" className="w-6 h-6"  />
                          </Link>
                          
                     </div>
